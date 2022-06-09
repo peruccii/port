6ba4d8b05d45b8cb5a08d9e58d6336cf74c02129
EDUARDO PERUCCI MARTINS DE SOUZA <22102325@CFP127ALUNO.LOCAL>
2022-06-09 11:28:44 -0300

    aaa

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    }
+  }
